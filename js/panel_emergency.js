605ff4cf04daa5cb6f329838ead5b64a555b5f8f
3rd-KaTZe <le_chat@perrin-lucas.com>
2015-02-18 17:51:59 +0100

    Implementation DCS Focus

--- js/panel_emergency.js
+++ js/panel_emergency.js
@@ -3,6 +3,7 @@ function panel_emergency_update(KaTZPit_data){
 	// Gestion des Alarmes
 	// Voyent Master warning
 	if (KaTZPit_data["Master_Warn"] ==1) {$("#VE_Alarm").fadeIn()} else {$("#VE_Alarm").fadeOut()}
+
 	
 	
 	// Debug
